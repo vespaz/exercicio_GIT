e9820dbe40888ca47b2a563c8f9f1442b8c89b12
victorhugo04 <victor.s.s.sntn@gmail.com>
2018-04-06 23:42:42 -0300

    V2 - cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -2,17 +2,17 @@
 <html lang='pt-br'>
 	<head>
 		<meta charset="UTF-8"/>
-		<title>Home</title>
+		<title>Cabeçalho</title>
 		<link rel="stylesheet" href="estilos.css"/>
 	</head>
 	<body>
 		<div>
-			<fieldset>
-				<legend></legend>
-				<a href="form_cadastro_pessoa">Cadastre-Se</a><br />	
-				<a href="imc.php">Calculadora IMC</a><br />
-				<!--<a href="">Calculadora</a>-->
-			</fieldset>
+			<center>
+				<a class="url" href="form_cadastro_pessoa.php">Cadastre-Se</a>	
+				<a class="url" href="imc.php">Calculadora IMC</a>
+				<a class="url" href="calculadora.php">Calculadora</a>
+				<a class="url" href="index.php">Home</a>
+			</center>
 		</div>
 		<?php
 			include "funcoes.php";
