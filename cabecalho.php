2453887305c4ffc9e7e8beefb5a59d2c8fcb362a
victorhugo04 <victor.s.s.sntn@gmail.com>
2018-04-06 21:49:00 -0300

    V1 - cabecalho.php

---
+++ cabecalho.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang='pt-br'>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>Home</title>
+		<link rel="stylesheet" href="estilos.css"/>
+	</head>
+	<body>
+		<div>
+			<fieldset>
+				<legend></legend>
+				<a href="form_cadastro_pessoa">Cadastre-Se</a><br />	
+				<a href="imc.php">Calculadora IMC</a><br />
+				<!--<a href="">Calculadora</a>-->
+			</fieldset>
+		</div>
+		<?php
+			include "funcoes.php";
+		?>
+	</body>
+</html>
